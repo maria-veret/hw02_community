043ad253786ab9c67e518498e31e68c505c0522e
Мария Веретенникова <veret.maria@gmail.com>
2022-03-04 01:53:27 +0300

    Homework_2 ver_2.1

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -9,6 +9,11 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'description')
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
+
 
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
